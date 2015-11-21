3598a0749a42cbff2c8de9386237151efc801825
Rob Wilson <robwilson101@gmail.com>
2015-11-21 20:51:46 +0000

    Create local-strategy

---
+++ api/config/passport.js
@@ -0,0 +1,30 @@
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("../models/user");
+
+module.exports = function (passport) {
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true,
+  }, function (req, email, password, done) {
+
+    // Find a user with supplied email
+    User.findOne({'local.email' : email}, function (err, user) {
+      if (err) return done(err, false, {message: "Something went wrong. Please try again in a few minutes."});
+      if (user) return done(null, false, {message: "This Email is already registered with HackJammer. Please login or use a new email address."});
+
+      // If no existing user, create the account
+      var newUser = new User();
+      newUser.local.email    = email;
+      newUser.local.password = User.encrypt(password);
+      newUser.save(function (err, user) {
+
+        // Error found
+        if (err) return done(err, false, {message: "Something went wrong. Please try again in a few minutes"});
+
+        return done(null, user);
+      });
+    });
+  }));
+};
