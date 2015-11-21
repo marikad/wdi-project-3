ed96049072cf5f5fae05ab413dc485da9ab52a4c
Rob Wilson <robwilson101@gmail.com>
2015-11-21 19:01:35 +0000

    Set up routes and config files

---
+++ api/config/routes.js
@@ -0,0 +1,11 @@
+// Require packages
+var express = require('express');
+var passport = require("passport");
+
+// Establish 'router'
+var router = express.Router();
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+
+module.exports = router
