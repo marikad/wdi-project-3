dae620e77b864096698c54bcbf6785dee8623dcd
Rob Wilson <robwilson101@gmail.com>
2015-11-21 20:45:33 +0000

    Added a user model

---
+++ api/models/user.js
@@ -0,0 +1,23 @@
+// Require Packages
+var mongoose = require("mongoose");
+var bcrypt   = require('bcrypt-nodejs');
+
+// Define a 'user' in a 'local' JSON object
+var userSchema = new mongoose.Schema({
+  local: {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+  }
+});
+
+// Create a statics to encrypt the password
+userSchema.statics.encrypt = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+// Create an instance method to validat any specific user
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
