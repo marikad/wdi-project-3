54a31d2c3f429b183a4f4eb681ab7973702acc7f
Rob Wilson <robwilson101@gmail.com>
2015-11-21 19:06:18 +0000

    Start working on controller

---
+++ api/controllers/authController.js
@@ -0,0 +1,20 @@
+// Require packages
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+// var User = require('../models/user');
+var secret = 'changeme'; // Change this
+
+function register(req, res, next) {
+  // Local strategy
+
+  // Set token
+
+  // return token
+
+  // return strategy
+};
+
+function login(req, res, next) {
+// Find user
+
+};
