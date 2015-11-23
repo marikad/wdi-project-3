36efaddf47049c4eb33815181242ef19d44c9070
webdev11 <marikadevan@gmail.com>
2015-11-23 10:24:26 +0000

    Adding front-end JS files. Remove EJS

---
+++ front-end/public/js/cities.js
@@ -0,0 +1,8 @@
+
+var uk = [
+	{name: "london", lat: 51.507351, lng: -0.127758},
+	{name: "manchester", lat: 53.480759, lng: -2.242631},
+	{name: "birmingham", lat: 52.497, lng: 13.396},
+	{name: "liverpool", lat: 53.408371, lng: -2.991573},
+	{name: "bristol", lat: 51.454513, lng: -2.587910}
+];
