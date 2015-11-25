98da5475a6d28921a5c312435145ef5167079513
KwakesProject <kwakes@me.com>
2015-11-25 17:06:41 +0000

    Adding array to loop over multiple keywords

--- api/lib/eventbrite.js
+++ api/lib/eventbrite.js
@@ -6,63 +6,39 @@ var mongoose = require("mongoose");
 mongoose.connect(config.database);
 
 var token    = process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN;
-var base_url = "https://www.eventbriteapi.com/v3/events/search/?token="
+var base_url = "https://www.eventbriteapi.com/v3/events/search/?q="
 var keywords = "hackathon";
-var url      = base_url + token + "&q=" + keywords;
 
-console.log("-1");
-console.log(url);
+for (var i = 0; i < 5; i++) {
+  var page_number = i
+  var url         = base_url + keywords +"&token=" + token + "&page=" + page_number;
 
-request(url, function (err, res, body) {
-  console.log("0");
-  if (err) return console.log(err);
-  if (res.statusCode !== 200)  return false;
+  request(url, function (err, res, body) {
+    if (err) return console.log(err);
+    if (res.statusCode == 200) {
+      var data = JSON.parse(body)
 
-  var data = JSON.parse(body)
-  // console.log(data)
+      // console.log(data)
 
-  var events = data.events;
-
-  console.log("1");
-  events.forEach(function(hackathon) {
-    if (hackathon.venue_id === null || !hackathon.venue_id) return false;
-
-    var venueURL = "https://www.eventbriteapi.com/v3/venues/" + hackathon.venue_id + "/?token=" + token;
-    // console.log(venueURL);
-
-    console.log("2");
-    request(venueURL, function(error, response, venueBody){
-      console.log("3");
-      console.log(error, response.body, venueBody);
+      var events  = data.events;
       
-      if (error) return console.log(error);
-      if (response.statusCode !== 200) return false;
+      for (i in events) {
+        if (events[i].start.timezone == 'Europe/London') {
+          console.log("Event title: " + events[i].name.text + " Description: " + events[i].description.text + "Date " + events[i].start.local + " Venue Id" + events[i].venue_id)
 
-      // var venue = JSON.parse(venueBody)
-      // console.log(venue);      
-    })
+          var newEvent          = new Event();
+          newEvent.title        = events[i].name.text;
+          // newEvent.city = events[i].city_name;
+          newEvent.description  = events[i].description.text;
+          // newEvent.location = events[i].venue_address;
+          newEvent.date         = events[i].start.local;
 
-    // request(venueURL, function (err, res, body){
-    //   if (err) return console.log(err);
-    //   if (res.statusCode !== 200) return false;
-
-    //   var venueData = JSON.parse(body)
-    //   var venue = venueData.venue
-
-    //   // var newEvent = new Event();
-    //   // newEvent.title = events[i].name.text;
-    //   // // newEvent.city = events[i].city_name;
-    //   // newEvent.description = events[i].description.text;
-    //   // // newEvent.location = events[i].venue_address;
-    //   // newEvent.date = events[i].start.local;
-
-    //   // newEvent.save(function (err, event) {
-    //   //   if (err) return res.status(500).json(err);
-    //   //   console.log(newEvent.title + " saved.")
-    //   // });
-
-    //   };
-    // })
-  })
-});
-
+          newEvent.save(function (err, event) {
+            if (err) return res.status(500).json(err);
+            console.log(newEvent.title + " saved.")
+          });
+        };
+      };
+    };
+  });
+}
--- api/lib/eventful.js
+++ api/lib/eventful.js
@@ -1,39 +1,51 @@
-var Event = require('../models/event');
-var request = require('request');
-var config = require('../config/config');
+var Event     = require('../models/event');
+var request   = require('request');
+var config    = require('../config/config');
 
-var mongoose = require("mongoose");
+var mongoose  = require("mongoose");
 mongoose.connect(config.database);
 
-var token = process.env.EVENTFUL_API_KEY;
-var base_url = "http://api.eventful.com/json/events/search?keywords="
-var keywords = "hackathon";
+var token     = process.env.EVENTFUL_API_KEY;
+var baseUrl  = "http://api.eventful.com/json/events/search?keywords=";
+var keywords  = ["hackathon", "javascript", "python"];
+var urls = []
 
-for (var i = 0; i < 18; i++) {
-  var page_number = i
-  var url      = base_url + keywords + "&app_key=" + token + "&page_number="+ page_number;
+for (var j = 0; j < keywords.length; j++) {
+  for (var i = 0; i < 19; i++) {
+    var url = baseUrl + keywords[j] + "&app_key=" + token + "&page_number="+ i
+    urls.push(url);
+  };
+};
 
-  request(url, function (err, res, body) {
+for (var k = 0; k < urls.length-1; k++) {
+
+  // BUG: Request does not act on each loop, instead it waits for the for loop to complete every iteration, and then executes urls.length - 1 times the last entry in urls.
+
+  // http://stackoverflow.com/questions/24710989/how-do-i-make-http-requests-inside-a-loop-in-nodejs
+
+  request(urls[k], function (err, res, body) {
+    console.log('Now requesting ' + urls[k]);
     if (err) return console.log(err);
     if (res.statusCode == 200) {
       var data = JSON.parse(body)
-
       var events = data.events.event;
+      // var category = urls[n].split("keywords=");
 
-      for (i in events) {
-        if (events[i].country_name == 'United Kingdom') {
-          console.log("Event title: " + events[i].title + " Location: " + events[i].city_name + " Venue: " + events[i].venue_address)
+      for (n in events) {
+        if (events[n].country_name == 'United Kingdom') {
+          // console.log("Event title: " + events[n].title + " Location: " + events[n].city_name + " Venue: " + events[n].venue_address)
 
           var newEvent = new Event();
-          newEvent.title = events[i].title;
-          newEvent.city = events[i].city_name;
-          newEvent.description = events[i].description;
-          newEvent.location = (events[i].venue_address + ", " + events[i].city_name);
-          newEvent.date = events[i].start_time;
+          newEvent.title = events[n].title;
+          newEvent.city = events[n].city_name;
+          newEvent.description = events[n].description;
+          newEvent.location = (events[n].venue_address + ", " + events[n].city_name);
+          newEvent.date = events[n].start_time;
+          // newEvent.category = ;
 
           newEvent.save(function (err, event) {
             if (err) return res.status(500).json(err);
-            console.log(newEvent.title + " saved.")
+            // console.log(newEvent.title + " saved.");
           });
         };
       };
